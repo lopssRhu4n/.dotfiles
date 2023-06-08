947f90e0bf1bff6c294b1cdd95dabe1c58111db3
Rhuan Lopes <rhuanlopes535@gmail.com>
2023-06-07 21:06:37 -0300

    Dunst, tms changes, polybar changes

--- bin/tmux-sessionizer.sh
+++ bin/tmux-sessionizer.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
@@ -20,4 +21,4 @@ if ! tmux has-session -t=$selected_name 2> /dev/null; then
     tmux new-session -ds $selected_name -c $selected
 fi
 
-tmux switch-client -t $selected_name
+tmux a 
